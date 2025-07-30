66ebe5fc600ac5b689a8289d501eb9c5572fabd6
sanjay kumar <100122199+sanjay3131@users.noreply.github.com>
2025-07-30 13:48:59 +0530

    Update Login.tsx

--- fontend/src/Pages/Login.tsx
+++ fontend/src/Pages/Login.tsx
@@ -88,7 +88,7 @@ const Login = () => {
           </div>
         </form>
         <a
-          href="https://ai-blogapp.onrender.com/auth/google"
+          href="https://ai-blogapp.onrender.com/api/auth/google"
           className="w-full flex justify-center items-center px-4"
         >
           <Button className="w-4/5">Login with Google</Button>
