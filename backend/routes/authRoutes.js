eb2209e94ee3a49e9fc0b09c734941c7f207c641
sanjay3131 <sanjay31ramuk@gmail.com>
2025-08-05 15:48:28 +0530

    google auth  route is changed to old format

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -120,7 +120,7 @@ router.get(
   asyncHandler(async (req, res) => {
     const token = generateToken(req.user._id, res);
     // Optionally redirect or send token
-    res.redirect(process.env); // or send JSON if API-only
+    res.redirect(process.env.FRONTEND_URL); // or send JSON if API-only
   })
 );
 export default router;
